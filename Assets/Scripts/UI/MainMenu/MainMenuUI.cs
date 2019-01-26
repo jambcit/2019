4a370b89b036947b6861f56d28fa8359b67a370e
Tyler Da Costa <dacosta-t@hotmail.com>
2019-01-26 15:15:35 -0800

    Main menu setup with room list and hosting/joining rooms.

---
+++ Assets/Scripts/UI/MainMenu/MainMenuUI.cs
@@ -0,0 +1,59 @@
+﻿namespace Home.UI
+{
+    using Home.Core;
+    using System;
+    using UnityEngine;
+    using UnityEngine.UI;
+    using Photon.Pun;
+    using Photon.Realtime;
+
+    public class MainMenuUI : MonoBehaviour
+    {
+        public static Action RoomCreated;
+
+        [SerializeField] private InputField charNameInput;
+
+        public void CreateRoomBtnClicked()
+        {
+            if (ValidPlayerName())
+            {
+                string roomName = charNameInput.text + "'s Room";
+                RoomOptions roomOptions = new RoomOptions { MaxPlayers = 4 };
+                PhotonNetwork.CreateRoom(roomName, roomOptions);
+            }
+        }
+
+        public void JoinRoomBtnClicked(GameObject obj)
+        {
+            if (ValidPlayerName())
+            {
+                TogglePanel(obj);
+            }
+        }
+
+        public bool ValidPlayerName()
+        {
+            return !string.IsNullOrEmpty(charNameInput.text);
+        }
+
+        public void TogglePanel(GameObject obj)
+        {
+            obj.SetActive(!obj.activeSelf);
+        }
+
+        public void ActivateElement(GameObject obj)
+        {
+            obj.SetActive(true);
+        }
+
+        public void DisableElement(GameObject obj)
+        {
+            obj.SetActive(false);
+        }
+
+        public void QuitApplication()
+        {
+            Application.Quit();
+        }
+    }
+}
