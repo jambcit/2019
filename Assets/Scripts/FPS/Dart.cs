4f830a09c338c9102cbdb716c02a26566495b904
Geoff McLennan <geoff.d.mclennan@gmail.com>
2019-01-26 15:38:58 -0800

    Added arcing to darts, removed unnecessary classes

--- Assets/Scripts/FPS/Dart.cs
+++ Assets/Scripts/FPS/Dart.cs
@@ -1,49 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Dart : MonoBehaviour
+﻿namespace MyNamespace
 {
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
 
-    private const float DART_VELOCITY = 30f;
-
-    private Rigidbody rigidBody;
-    private Vector3 startPosition;
-    private Vector3 startDirection;
-    private bool isStuck;
-
-    // Start is called before the first frame update
-    void Start()
+    public class Dart : MonoBehaviour
     {
-        Debug.Log("Dart Start");
-        rigidBody = GetComponent<Rigidbody>();
-        rigidBody.AddForce(transform.up * DART_VELOCITY, ForceMode.Impulse);
-        startPosition = transform.position;
-        startDirection = transform.up;
-        isStuck = false;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        private const float DART_VELOCITY = 30f;
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log("Collided");
-    }
+        private Rigidbody rigidBody;
+        private bool isStuck;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("Entered trigger");
-        if (other.GetComponent<StickyObject>() != null)
+        // Start is called before the first frame update
+        void Start()
         {
-            isStuck = true;
-            transform.parent = other.transform.parent;
-            rigidBody.isKinematic = true;
+            rigidBody = GetComponent<Rigidbody>();
+            rigidBody.AddForce(transform.up * DART_VELOCITY, ForceMode.Impulse);
+            isStuck = false;
         }
-    }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (!isStuck)
+            {
+                transform.LookAt(transform.position - Vector3.Cross(rigidBody.velocity, transform.right));
+            }
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            Debug.Log("Collided");
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+
+        }
 
 
+    } 
 }
