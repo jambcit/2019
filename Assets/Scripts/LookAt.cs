13b045d047f9b6e79bed40cffda78bc4d84a88ec
Shawn <sdf1342@gmail.com>
2019-01-26 12:51:30 -0800

    Added simple lookat target

---
+++ Assets/Scripts/LookAt.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SandboxGUILookAt : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(target != null)
+        {
+            
+            transform.LookAt(new Vector3(target.position.x, target.position.y, target.position.z));
+        }
+    }
+}
