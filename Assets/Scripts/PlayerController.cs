cd66fe4390bbf43e2ff9bdc5eec54d621a6a9d69
Joshua Abe <joshua.t.abe@gmail.com>
2019-01-26 00:28:31 -0800

    Added high level input handling for different pawn types

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Home.Core
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private Pawn controlledPawn;
+
+        private void Start()
+        {
+            if (controlledPawn != null)
+            {
+                controlledPawn.Attach(this);
+            }
+        }
+
+        public void AttachPawn(Pawn pawn)
+        {
+            if (controlledPawn != null)
+            {
+                controlledPawn.Detach();
+            }
+            controlledPawn = pawn;
+            pawn.Attach(this);
+        }
+    }
+}
---
+++ Assets/Scripts/Sandbox/SandboxPawn.cs
@@ -0,0 +1,19 @@
+﻿using Home.Core;
+using UnityEngine;
+
+namespace Home.Sandbox
+{
+    public class SandboxPawn : Pawn
+    {
+        void Start()
+        {
+            UpdateActions += MoveMe;
+            // LateUpdateActions += YourLateFunc;
+        }
+
+        private void MoveMe()
+        {
+            transform.Translate(Vector3.forward);
+        }
+    }
+}
