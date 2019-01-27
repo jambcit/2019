1ddc316a2cb23913c9a93ad4db33cf37e0b4c096
Shawn <sdf1342@gmail.com>
2019-01-26 20:00:48 -0800

    Throw to Morris

---
+++ Assets/Scripts/HideAndSeek/HideAndSeekPawn.cs
@@ -0,0 +1,15 @@
+﻿using Home.Core;
+using UnityEngine;
+
+namespace Home.HideAndSeek
+{
+    public class HideAndSeekPawn : Pawn
+    {
+        HideAndSeekCharacterMovement hideAndSeekCharacterMovement;
+
+        public override void Initialize()
+        {
+            hideAndSeekCharacterMovement = new HideAndSeekCharacterMovement();
+        }
+    }
+}
